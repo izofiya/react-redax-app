3fa78fdb23a7a838cf4cb46e89c656536801988a
Zoya Pleskatsevich <z.pleskatsevich@godeltech.com>
2020-06-18 22:29:19 +0300

    add project

---
+++ src/store/actions/counter.js
@@ -0,0 +1,29 @@
+import * as actionTypes from "./actionsTypes";
+
+export const increment = (value) => {
+  return {
+    type: actionTypes.INCREMENT,
+    val: value,
+  };
+};
+
+export const decrement = (value) => {
+  return {
+    type: actionTypes.DECREMENT,
+    val: value,
+  };
+};
+
+export const add = (value) => {
+  return {
+    type: actionTypes.ADD,
+    val: value,
+  };
+};
+
+export const subtract = (value) => {
+  return {
+    type: actionTypes.SUBTRACT,
+    val: value,
+  };
+};
---
+++ src/store/reducers/counter.js
@@ -0,0 +1,25 @@
+import * as actionTypes from "../actions/actionsTypes";
+import { updateObject } from "../utility";
+
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.INCREMENT:
+      // const newState = Object.assign({}, state);
+      // newState.counter = state.counter + action.val;
+      // return newState;
+      return updateObject(state, { counter: state.counter + action.val });
+    case actionTypes.DECREMENT:
+      return updateObject(state, { counter: state.counter - action.val });
+    case actionTypes.ADD:
+      return updateObject(state, { counter: state.counter + action.val });
+    case actionTypes.SUBTRACT:
+      return updateObject(state, { counter: state.counter - action.val });
+  }
+  return state;
+};
+
+export default reducer;
